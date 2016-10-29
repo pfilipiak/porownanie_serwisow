d23ece81b65ddbc565c01b4e59b7779d45c2a600
pfilipiak <pawel.filipiak89@gmail.com>
2016-10-29 17:27:23 +0200

    cos nowego

--- porownanie_serwisow/src/sql_files/imports/ceneo/import_produkt_ceneo.sql
+++ porownanie_serwisow/src/sql_files/imports/ceneo/import_produkt_ceneo.sql
@@ -1,6 +1,7 @@
 create table import_produkt_ceneo(
 id bigserial primary key,
 id_paczka bigint references d_import_package(id) restrict update cascade delete cascade,
+id_status_import bigint references d_import_status(id) restrict update cascade delete cascade,
 
 domain text,
 keyword text,
